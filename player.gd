454e5df8e8d0812b9e91d9b0f4e86a18aa918fd4
dawid <dawid.galeziewski@gmail.com>
2024-04-27 12:58:05 +0200

    add player movements and animations

--- player.gd
+++ player.gd
@@ -11,4 +11,37 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO; # player movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1;
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1;
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1;
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1;
+		
+	var is_player_moving = velocity.length() > 0;
+	
+	if is_player_moving:
+		# we use normalized so that if player presess top and down the oucome wont be (1,1) * 400. This would let us move faster diagonally 
+		velocity = velocity.normalized() * speed; 
+		# play animation for movement
+		var is_player_moving_horizontaly = velocity.x != 0;
+		var is_player_moving_verticaly = velocity.y != 0;
+	
+		if is_player_moving_horizontaly:
+			$AnimatedSprite2D.animation = "walk";
+			var is_player_moving_left = velocity.x < 0;
+			$AnimatedSprite2D.flip_h = is_player_moving_left;
+		elif is_player_moving_verticaly:
+			$AnimatedSprite2D.animation = "up";
+			var is_player_moving_down = velocity.y > 0;
+			$AnimatedSprite2D.flip_v = is_player_moving_down;
+			$AnimatedSprite2D.play();
+	else:
+		$AnimatedSprite2D.stop();
+		
+	position += velocity * delta;
+	# we use clamp to prevent user from leaving the screen
+	position = position.clamp(Vector2.ZERO, screen_size);
